fdb32d45012e5b80e70c44a0c25cb8c06c5d9410
Le Huynh Ngoc Thuy <kate.le0248@gmail.com>
2013-08-01 16:46:21 +0700

    user model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VenShop::Application.config.secret_token = '5172947ea3502ead106b55a1b0ac81465a4f786deb554d7fa54237ea4488d465df135f924c94c8b8ba2de397cf6b3f36ad9d73b7b7fa4e0c20e17a59ace94593'
